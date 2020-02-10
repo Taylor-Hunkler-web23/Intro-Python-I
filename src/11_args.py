4f30b871a086568f1c1439fcf28b6180b271a70b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-02-10 18:54:22 -0500

    Solved first problem in 11_args

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(first, second):
+    return first + second
 
 print(f1(1, 2))
 
@@ -13,8 +15,14 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*num):
+    sum = 0
+    
+    for n in num:
+        sum = sum + n
 
-print(f2(1))                    # Should print 1
+
+# print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
